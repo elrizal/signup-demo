02159469c2061ad481437bcfa029ff01449c19d6
elrizal <erinrizal@Erins-MacBook-Pro.local>
2019-02-21 23:31:37 -0500

    signup

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -30,9 +30,6 @@ router.get("/login", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/login.html"));
 });
 
-// Here we've add our isAuthenticated middleware to this route. If a user who is
-// not logged in tries to access this route they will be redirected to the
-// signup page
 router.get("/members", isAuthenticated, function (req, res) {
   res.sendFile(path.join(__dirname, "../public/members.html"));
 });
